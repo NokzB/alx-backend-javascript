59a8ad9d89992bc4e1f012115c723e4d360205e4
Nokuphila <nnblb58@gmail.com>
2024-12-16 08:52:31 +0000

    task 1

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import ClassRoom from './0-classroom';
+/**
+ * Creates an array of classrooms with a specific size.
+ * @returns An array of 3 ClassRoom objects.
+ */
+export default function initializeRooms() {
+  return [19, 20, 34].map((size) => new ClassRoom(size));
+}
